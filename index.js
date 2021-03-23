dd208e4a572aeac0319c615c8964e9708f6157aa
joey jiron <joeyjiron@microsoft.com>
2021-03-23 23:34:18 +0000

    added env setting for local settings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
+const path = require('path');
 
 class MissingEnvVarsError extends Error {
   constructor(...args) {
@@ -11,19 +12,28 @@ class MissingEnvVarsError extends Error {
   }
 }
 
-function getExample(path) {
+function getExample(pathToExampleSettings) {
   try {
-    return require(path);
+    return require(pathToExampleSettings);
   } catch (error) {
     if (error.code === 'MODULE_NOT_FOUND') {
       throw new MissingEnvVarsError(`Example file does not exist. Make sure that the example file exist at location ${error.moduleName} or that you specified the correct example path.`);
     }
 
     if (error instanceof SyntaxError) {
-      throw new MissingEnvVarsError(`Example file ${path} must be a JSON object.\nError message: ${error.message}`);
+      throw new MissingEnvVarsError(`Example file ${pathToExampleSettings} must be a JSON object.\nError message: ${error.message}`);
     }
 
     throw error;
+  }
+}
+
+function getLocalSettings() {
+  try {
+    const pathToLocalSettings = path.resolve(process.cwd(), 'local.settings.json');
+    return require(pathToLocalSettings);
+  } catch (error) {
+    return undefined;
   }
 }
 
@@ -34,13 +44,26 @@ function getExample(path) {
  * @param {string} options.example - path to the example file
  */
 function config(options = {}) {
+  const debug = (...args) => options.debug && console.debug(...args);
+
+  debug('process.env before anything', process.env);
+
   const example = options.example || './local.settings.example.json';
   const exampleJSON = getExample(example);
+  const localSettings = getLocalSettings();
   const missingVars = [];
 
   if (!exampleJSON.Values || typeof exampleJSON.Values !== 'object') {
     throw new MissingEnvVarsError(`Example json must contain a "Values" object but received:\n${exampleJSON.Values}`);
   }
+
+  debug('found localSettings', localSettings);
+
+  Object.keys(localSettings.Values).forEach((key) => {
+    process.env[key] = localSettings.Values[key];
+  });
+
+  debug('process.env after localSettings loaded', process.env);
 
   Object.keys(exampleJSON.Values).forEach((key) => {
     if (typeof exampleJSON.Values[key] !== 'string') {
--- index.test.js
+++ index.test.js
@@ -1,25 +1,24 @@
-const { config } = require('./index');
-
+/* eslint-disable global-require */
 const LOCAL_SETTINGS_EXAMPLE_PATH = './local.settings.example.json';
-
-function mockExampleJson(mockJson, modulePath = LOCAL_SETTINGS_EXAMPLE_PATH) {
-  jest.mock(modulePath, () => mockJson);
-}
+const LOCAL_SETTINGS_PATH = './local.settings.json';
 
 describe('config', () => {
+  let config;
   beforeEach(() => {
     jest.resetModules();
     process.env = {};
+
+    config = require('./index').config;
   });
 
   it('should succeed when all environment variables exist in process.env', async () => {
-    mockExampleJson({
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
       Values: {
         var1: 'exampleVar1',
         var2: 'exampleVar2',
         var3: 'exampleVar3',
       },
-    });
+    }));
 
     process.env = {
       var1: 'actualVar1',
@@ -31,14 +30,13 @@ describe('config', () => {
   });
 
   it('should succeed when using a custom example file and all variables exist in process.env', async () => {
-    const example = './local.settings.custom.json';
-    mockExampleJson({
+    jest.mock('./local.settings.custom.json', () => ({
       Values: {
         var3: 'exampleVar3',
         var4: 'exampleVar4',
         var5: 'exampleVar5',
       },
-    }, example);
+    }));
 
     process.env = {
       var3: 'actualVar3',
@@ -46,18 +44,57 @@ describe('config', () => {
       var5: 'actualVar5',
     };
 
-    expect(() => config({ example })).not.toThrow();
+    expect(() => config({ example: './local.settings.custom.json' })).not.toThrow();
   });
 
+  it('should set environment variables from local.settings.json when default config is used', async () => {
+    jest.mock(LOCAL_SETTINGS_PATH, () => ({
+      Values: {
+        var6: 'actualVar6',
+        var7: 'actualVar7',
+        var8: 'actualVar8',
+      },
+    }));
+
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
+      Values: {
+        var6: 'exampleVar6',
+        var7: 'exampleVar7',
+        var8: 'exampleVar8',
+      },
+    }));
+
+    process.env = {};
+
+    expect(() => config()).not.toThrow();
+    expect(process.env).toEqual({
+      var6: 'actualVar6',
+      var7: 'actualVar7',
+      var8: 'actualVar8',
+    });
+  });
+
+  it.skip('should NOT set environment variables when env variables already exist', async () => {});
+
+  it.skip('should not throw an error when path to default local.settings.json does not exist', async () => {});
+
+  it.skip('should throw an error when path to custom local.settings.json does not exist', async () => {});
+
+  it.skip('should throw an error when local.settings.json is not a json object', async () => {});
+  it.skip('should throw an error when local.settings.json "Values" is undefined', async () => {});
+  it.skip('should throw an error when local.settings.json "Values" is null', async () => {});
+  it.skip('should throw an error when local.settings.json "Values" is not an object', async () => {});
+  it.skip('should throw an error when local.settings.json "Values" has values that are not strings', async () => {});
+
   it('should throw an error when environment variables are missing', async () => {
-    mockExampleJson({
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
       Values: {
         var1: 'var1',
         var2: 'var2',
         var3: 'var3',
         var4: 'var4',
       },
-    });
+    }));
 
     process.env = {
       var1: 'var1',
@@ -95,14 +132,13 @@ describe('config', () => {
   });
 
   it('should throw an error when example json "Values" is undefined', async () => {
-    const mockJson = {
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
       ShouldBeValues: {
         var3: 'exampleVar3',
         var4: 'exampleVar4',
         var5: 'exampleVar5',
       },
-    };
-    mockExampleJson(mockJson);
+    }));
 
     process.env = {
       var3: 'actualVar3',
@@ -114,10 +150,9 @@ describe('config', () => {
   });
 
   it('should throw an error when example json "Values" is null', async () => {
-    const mockJson = {
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
       Values: null,
-    };
-    mockExampleJson(mockJson);
+    }));
 
     process.env = {
       var3: 'actualVar3',
@@ -129,10 +164,9 @@ describe('config', () => {
   });
 
   it('should throw an error when example json "Values" is not an object', async () => {
-    const mockJson = {
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
       Values: 1234, // number here, should be an object an error out
-    };
-    mockExampleJson(mockJson);
+    }));
 
     process.env = {
       var3: 'actualVar3',
@@ -140,23 +174,25 @@ describe('config', () => {
       var5: 'actualVar5',
     };
 
-    expect(() => config()).toThrow(`Example json must contain a "Values" object but received:\n${mockJson.Values}`);
+    expect(() => config()).toThrow('Example json must contain a "Values" object but received:\n1234');
   });
 
   it('should throw an error when example json has values that are not strings', async () => {
-    const mockJson = {
+    jest.mock(LOCAL_SETTINGS_EXAMPLE_PATH, () => ({
       Values: {
         var1: 'exampleVar1',
         var2: 1234, // should not be a number and should throw an error
       },
-    };
-    mockExampleJson(mockJson);
+    }));
 
     process.env = {
       var1: 'actualVar3',
       var2: 'actualVar4',
     };
 
-    expect(() => config()).toThrow(`Example json "Values" must only have values which are strings but received:\n${JSON.stringify(mockJson.Values, null, 2)}`);
+    expect(() => config()).toThrow(`Example json "Values" must only have values which are strings but received:\n${JSON.stringify({
+      var1: 'exampleVar1',
+      var2: 1234, // should not be a number and should throw an error
+    }, null, 2)}`);
   });
 });
